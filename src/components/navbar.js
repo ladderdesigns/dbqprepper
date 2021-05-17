526283efe1acda03df9382a1ada6e475edc94a40
Jacob Patel <jseanpatel@gmail.com>
2021-05-16 23:19:48 -0700

    addressed fontawesome flashing issue

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,11 +3,18 @@ import { Link } from "gatsby";
 import { Icon } from "@iconify/react";
 import koFi from "@iconify/icons-simple-icons/ko-fi";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+
+config.autoAddCss = false; /* eslint-disable import/first */
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
-
-faNewspaper.autoAddCss = false
 
 library.add(faNewspaper);
 
